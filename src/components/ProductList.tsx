4b5c748cf0899e20589e528a39c8fcf2cfbbaa1f
technova675 <gameops25@gmail.com>
2025-05-13 11:37:16 +0530

    Update ProductList.tsx

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ProductItem from './ProductItem'
 
 const products = [
-  { src: '/src/assets/images/chain.png', alt: 'Chain' },
-  { src: '/src/assets/images/bracelet.png', alt: 'Snake' },
-  { src: '/src/assets/images/ring.png', alt: 'Ring' },
+  { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWFr1DPJvyu3Tt79mxN1Co1P9R-H5Em6FKqZciIFpI4P4Ae-d', alt: 'Chain' },
+  { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGd4OhMlNrarvpVpClKdLSKaH8ICUuC9PAJ5bh2p-Wjr6akXW7', alt: 'Snake' },
+  { src: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR-4GmXnzYal477OxjL6vnru7PQZilUd1fFS-6sbTW6EADCU_EK', alt: 'Ring' },
 ]
 
 const ProductList: React.FC = () => (
