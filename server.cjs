724346692cb56fa74e66c1b1830ef6439fad93da
viroffical <krishnavirnlu@gmail.com>
2025-05-17 02:53:26 +0530

    conflict changes

--- server.cjs
+++ server.cjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express")
 const Razorpay = require("razorpay")
 const cors = require("cors")
@@ -42,49 +41,4 @@ app.post("/order", async (req, res) => {
 
 app.listen(PORT, () => {
     console.log("Listening on port", PORT)
-=======
-const express = require("express")
-const Razorpay = require("razorpay")
-const cors = require("cors")
-require("dotenv").config();
-
-// import express from 'express'
-// import Razorpay from 'razorpay'
-// import cors from 'cors'
-// import dotenv from 'dotenv'
-
-// dotenv.config();
-
-
-const app = express();
-const PORT = process.env.PORT
-
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cors());
-
-app.post("/order", async (req, res) => {
-    try {
-        const razorpay = new Razorpay({
-        key_id: process.env.RAZORPAY_KEY_ID,
-        key_secret: process.env.RAZORPAY_KEY_SECRET
-    })
-
-    const options = req.body;
-    const orders = await razorpay.orders.create(options);
-
-    if(!orders) {
-        return res.status(500).send("Error")
-    }
-
-    res.json(orders)
-    } catch (error) {
-        res.status(500).send("Error")
-    }
-   
-})
-
-app.listen(PORT, () => {
-    console.log("Listening on port", PORT)
->>>>>>> 1a30083cb2da827df8bc64b5fd53f98dc7000b20
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -7,14 +6,4 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
->>>>>>> 1a30083cb2da827df8bc64b5fd53f98dc7000b20
 })
